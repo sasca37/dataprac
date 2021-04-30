ece32e49c2be60bd3909ed58d20d3b931851472d
sasca37 <sasca37@naver.com>
2021-04-30 15:54:02 +0900

    sa

--- src/main/java/com/cos/data/DataApplication.java
+++ src/main/java/com/cos/data/DataApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
+  
 @SpringBootApplication
 public class DataApplication {
 
